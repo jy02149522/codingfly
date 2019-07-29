de0630797cc1614691cede903a11abb2263d499b
hanjun <hanjun@ilarge.cn>
2019-07-29 22:52:14 +0800

    增加springcloud-gateway

---
+++ gateway/src/main/java/org.codingfly.gateway/config/RouteConfig.java
@@ -0,0 +1,69 @@
+package org.codingfly.gateway.config;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.util.pattern.PathPatternParser;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ *  跨域配置
+ */
+@Configuration
+public class RouteConfig {
+
+    //这里为支持的请求头，如果有自定义的header字段请自己添加（不知道为什么不能使用*）
+    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN,token,username,client";
+    private static final String ALLOWED_METHODS = "*";
+    private static final String ALLOWED_Expose = "*";
+    private static final String MAX_AGE = "18000L";
+
+
+    @Bean
+    public WebFilter corsWebFilter() {
+
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                ServerHttpResponse response = ctx.getResponse();
+                HttpHeaders headers = response.getHeaders();
+                List<String> objOrigin = request.getHeaders().get("Origin");
+                if (objOrigin == null || objOrigin.size() == 0) {
+                    headers.add("Access-Control-Allow-Origin", "null");
+                } else {
+                    headers.add("Access-Control-Allow-Origin", objOrigin.get(0));
+                }
+                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+                headers.add("Access-Control-Max-Age", MAX_AGE);
+                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+                headers.add("Access-Control-Expose-Headers", ALLOWED_Expose);
+                headers.add("Access-Control-Allow-Credentials", "true");
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(ctx);
+        };
+    }
+
+}
